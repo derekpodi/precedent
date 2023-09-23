d8dcfb882458b25f760a2fe2585ff7380056285a
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2023-09-22 17:57:58 -0700

    Header, footer component changes. Dummy page

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,10 +6,14 @@ import useScroll from "@/lib/hooks/use-scroll";
 import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
+import { usePathname} from 'next/navigation'
+
 
 export default function NavBar({ session }: { session: Session | null }) {
   const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
+  const pathname = usePathname();
+
 
   return (
     <>
@@ -23,15 +27,15 @@ export default function NavBar({ session }: { session: Session | null }) {
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-display text-2xl">
-            <Image
-              src="/logo.png"
-              alt="Precedent logo"
-              width="30"
-              height="30"
-              className="mr-2 rounded-sm"
-            ></Image>
-            <p>Precedent</p>
+            <p>Nav Title</p>
           </Link>
+
+          <div>
+          <Link href={pathname} className="flex items-center font-display text-2xl">
+            <p>/Home{pathname}</p>
+          </Link>
+          </div>
+          
           <div>
             {session ? (
               <UserDropdown session={session} />
