9e02fc4fd524e259c0e035b313ca9efbe1c80997
Derek Podimatis <derek@Dereks-MacBook-Pro.local>
2023-10-03 23:24:57 -0700

    Pages title xl, 4xl column size

--- app/blog/[[...slug]]/page.tsx
+++ app/blog/[[...slug]]/page.tsx
@@ -2,9 +2,9 @@ import { Links } from "@/components/links";
 
 export default function Page({ params }: { params: { slug: string } }) {
     return (
-      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-      My Post: {params.slug}
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed explicabo, deleniti fuga eligendi velit officia error non modi facilis quos mollitia saepe! Quibusdam debitis ab hic fugit distinctio ipsa? Iure. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae non, error labore dolores vero odio nesciunt impedit consequuntur hic? Ad, hic optio? Autem blanditiis molestias veritatis debitis officia placeat.</p>
+      <div className="z-10 w-full max-w-4xl">
+        <p className="text-xl">My Post: {params.slug}</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed explicabo, deleniti fuga eligendi velit officia error non modi facilis quos mollitia saepe! Quibusdam debitis ab hic fugit distinctio ipsa? Iure. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi molestiae non, error labore dolores vero odio nesciunt impedit consequuntur hic? Ad, hic optio? Autem blanditiis molestias veritatis debitis officia placeat.</p>
       </div>
     );
   }
